9a8c0577bf28451e6d5f164be1480207413839f6
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-09-08 20:59:42 +0530

    Implement Navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Dashbaord() {
   return (
     <>
       <h1>Protected PAge</h1>
-      <UserButton />
     </>
   );
 }
