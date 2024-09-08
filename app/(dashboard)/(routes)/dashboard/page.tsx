3cb2a653a4f3a9365005c81260ea2a844413f392
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-09-08 20:41:07 +0530

    Setup Routes for Auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Dashbaord() {
   return (
     <>
       <h1>Protected PAge</h1>
+      <UserButton />
     </>
   );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <h1>LAnding PAge</h1>;
+  return (
+    <>
+      <h1>LAnding PAge</h1>
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
